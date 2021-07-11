936a7b234519ec0ecb11a87390fcfd3b704573a7
Zora Santos <zoranildo22@gmail.com>
2021-07-11 16:11:04 -0300

    First commit

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { Main } from '.';
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'Title Default',
+    description: 'Description Default'
+  }
+} as Meta;
+
+export const Basic: Story = (args) => <Main {...args} />;
+Basic.args = {
+  title: 'React Avançado',
+  description: 'TypeScript, ReactJS, NextJS e Styled Components'
+};
+
+export const Default: Story = (args) => <Main {...args} />;
