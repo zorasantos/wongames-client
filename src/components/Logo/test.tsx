55cbfdb487620f8795e147b5e9279e9217e084dc
Zora Santos <zoranildo22@gmail.com>
2021-07-13 00:31:39 -0300

    Logo test

---
+++ src/components/Logo/test.tsx
@@ -0,0 +1,23 @@
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from '../../utils/tests/helpers';
+
+import { Logo } from '.';
+
+describe('<Logo />', () => {
+  it('should render a white label by default', () => {
+    // renderizar o componente `render`
+    // selecionar o elemento a ser testado `screen`
+    // expect - análise (espero que renderize a logo branca por padrão)
+    renderWithTheme(<Logo />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    });
+  });
+
+  it('should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    });
+  });
+});
