a25dd3f21dc7526942be90ac7e1c2c4d42b7d097
Zora Santos <zoranildo22@gmail.com>
2021-07-14 00:11:37 -0300

    Conclui Component Logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react';
 import { renderWithTheme } from '../../utils/tests/helpers';
+import 'jest-styled-components';
 
 import { Logo } from '.';
 
@@ -19,5 +20,30 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#030517'
     });
+  });
+
+  it('should render a normal logo', () => {
+    renderWithTheme(<Logo size="normal" />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    });
+  });
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    });
+  });
+
+  it('should render a bigger logo without text mobile if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    );
   });
 });
