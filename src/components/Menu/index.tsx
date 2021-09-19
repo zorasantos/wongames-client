c23b749c6ae94a826e342845a4a14d29625f1e7d
Zora Santos <zoranildo22@gmail.com>
2021-09-19 07:56:04 -0300

    First test menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,24 +2,24 @@ import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2';
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart';
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search';
 
-import { Logo } from 'components/Logo';
+import { Logo } from '../../components/Logo';
 
 import * as S from './styles';
 
 export const Menu = () => (
   <S.Wrapper>
     <S.IconWrapper>
-      <MenuIcon />
+      <MenuIcon aria-label="Open Menu" />
     </S.IconWrapper>
     <S.LogoWrapper>
       <Logo hideOnMobile />
     </S.LogoWrapper>
     <S.MenuGroup>
       <S.IconWrapper>
-        <SearchIcon />
+        <SearchIcon aria-label="Search" />
       </S.IconWrapper>
       <S.IconWrapper>
-        <ShoppingCartIcon />
+        <ShoppingCartIcon aria-label="Open Shopping Cart" />
       </S.IconWrapper>
     </S.MenuGroup>
   </S.Wrapper>
--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,13 +1,13 @@
-import { screen, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from '../../utils/tests/helpers';
 import { Menu } from './index';
 
 describe('<Menu />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Menu />);
+  it('should render the menu', () => {
+    renderWithTheme(<Menu />);
 
-    expect(
-      screen.getByRole('heading', { name: /Menu/i })).toBeInTheDocument();
-
-    expect(container.firstChild).toMatchSnapshot();
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument();
   });
 });
